bfa5cb0d2d90321f00a54a986de988dfaa7d3aca
Bailey <bailey@useaquasuite.com>
2024-09-24 20:01:36 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const h1 = document.getElementById('prompt');
+    const buttons = document.querySelectorAll('button');
+    console.log(buttons);
+    const computerOptions = document.querySelectorAll('button');
+
+    function disableButtons() {
+        for (let i = 0; i < 3; i++) {
+            buttons[i].disabled = true;
+            buttons[i].style.display = "none";
+        }
+        h1.style.display = "none";
+    }
+
+    for (let i = 0; i < 3; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const userChoice = button.id
+            console.log('You chose: ', userChoice);
+            const randomChoice = Math.floor(Math.random() * computerOptions.length);
+            const computerChoice = computerOptions[randomChoice].id;
+            console.log('Computer chose: ', computerChoice); 
+
+            disableButtons();
+        });
+    }
+});
