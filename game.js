d119ae3e45e78897a03dc257a718dddee7c780a8
Bailey <bailey@useaquasuite.com>
2024-09-25 20:38:50 -0700

    add animation

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(buttons);
     const computerOptions = document.querySelectorAll('button');
     const result = document.getElementById('result');
+    const shuffle = document.querySelectorAll('li');
+
+    const BASE_TIME = 100;
 
     function disableButtons() {
         for (let i = 0; i < 3; i++) {
@@ -13,10 +16,37 @@ document.addEventListener('DOMContentLoaded', function () {
         h1.style.display = "none";
     }
 
+    let counter = 0
+
+   function hideListItems() {
+        for (i = 0; i < 3; i++) {
+            shuffle[i].classList.remove('active');
+        }
+   }
+
+    function animation() {
+        hideListItems();
+        shuffle[counter].classList.add('active');
+        if (counter === 2) {
+            counter = 0;
+        } else {
+            counter++;
+        }
+    } 
+
+    function animateShuffle() {
+        for (i = 0; i < 3; i++) {
+            // if (i === 0) {
+            //     shuffle[i].classList.add('active');
+            // }
+            setTimeout(animation, (i + 1) * BASE_TIME);
+      }
+    }
+
     function displayResult() {
         result.style.display = "grid";
     }
-
+  
     for (let i = 0; i < 3; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
@@ -27,31 +57,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const randomChoice = Math.floor(Math.random() * computerOptions.length);
             let computerChoice = computerOptions[randomChoice].id;
-            computerChoice = computerChoice.toString();
             console.log('Computer chose: ', computerChoice);
             document.getElementById('computer-choice').innerHTML = computerChoice;
 
             disableButtons();
-            displayResult();
+            animateShuffle();
+            setTimeout(animateShuffle, (shuffle.length + 1) * BASE_TIME);
+            
+            function displayComputerChoice() {
+                hideListItems();
+                document.getElementById(computerChoice).style.display = "grid";
+            }
+
+            setTimeout(displayComputerChoice, 10 * BASE_TIME);
+
+            setTimeout(displayResult, (BASE_TIME * 9) + BASE_TIME);
 
             function winLossResult(userChoice, computerChoice) {
                 if (userChoice === computerChoice) {
                     return "It's a tie! Try again";
                 }
-                if (userChoice === 'rock') {
-                    if (computerChoice === 'scissors') {
+                if (userChoice === 'Rock') {
+                    if (computerChoice === 'Scissors') {
                         return "You won! Rock beats scissors.";
                     } else {
                         return "You lose! Paper beats rock.";
                     }
-                } else if (userChoice === 'paper') {
-                    if (computerChoice === 'rock') {
+                } else if (userChoice === 'Paper') {
+                    if (computerChoice === 'Rock') {
                         return "You won! Paper beats rock.";
                     } else {
                         return "You lose! Scissors beats paper.";
                     }
-                } else if (userChoice === 'scissors') {
-                    if (computerChoice === 'paper') {
+                } else if (userChoice === 'Scissors') {
+                    if (computerChoice === 'Paper') {
                         return "You won! Scissors beats paper.";
                     } else {
                         return "You lose! Rock beats paper.";
